602a40d8d4fc630597c747f04594f9d249270101
Tomasz Plonski <tplonski@outlook.com>
2023-02-25 13:15:59 +0100

    Project ready

---
+++ src/Game.java
@@ -0,0 +1,162 @@
+import java.util.Scanner;
+
+public class Game {
+
+    private String movieTitleToGuess;
+    private String hiddenTittle;
+    private int points = 10;
+
+    private String alreadyTypedLetters = "";
+    private String alreadyTypedWrongLetters = "";
+
+    public Game(String movieTitleToGuess) {
+        this.movieTitleToGuess = movieTitleToGuess;
+        this.hiddenTittle = hideTittleUderscore(movieTitleToGuess);
+//        this.points = points;
+    }
+
+    public boolean startGame() {
+        System.out.println("Program draw from database one movie title and hide letters from you.");
+        System.out.println("Please guess full tittle by guessing letters. you have 10 shots.");
+//        System.out.println("Title: " + hiddenTittle);
+
+        boolean isGameOver = false;
+
+        while (!isGameOver) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Title: " + hiddenTittle);
+            System.out.println("Please type one letter:");
+            String typedLetter = scanner.next();
+//            alreadyTypedLetters += typedLetter;
+
+//            sprawdz czy to jest litera jesli nie to blad
+//            sprawdz czy juz ta litera nie zostala wprowadzona i jesli nie to przejd do zagadywanai dopiero
+            //        check if typed sign is a letter
+            if (wasAlreadyTyped(typedLetter)) {
+                System.out.println("Letter '" + typedLetter + "' was already typed (you do not lose point).");
+                continue;
+            }
+
+            if (guessLetterInTitle(typedLetter)) {
+                if (isTitleGuessed()) {
+//                    break;
+                    return true;
+                }
+                System.out.print("You have guessed! '" + typedLetter + "' is in the title. ");
+                if (points == 1) {
+                    System.out.println("You have still ONLY ONE point!.");
+                } else {
+                    System.out.println("You have still " + points + " points.");
+                    }
+                System.out.println("(Already typed WRONG letters are: " + alreadyTypedWrongLetters +")");
+
+            } else {
+                System.out.print("You have NOT guessed and lose 1 point! '" + typedLetter + "' is NOT in the tittle. ");
+                if (points == 1) {
+                    System.out.println("Now you have ONLY ONE point!.");
+                } else {
+                    System.out.println("Now you have " + points + " points.");
+                }
+
+                System.out.println("(Already typed WRONG letters are: " + alreadyTypedWrongLetters +")");
+
+                if (points == 0) {
+//                    break;
+                    return false;
+                }
+            }
+
+//            System.out.println(hiddenTittle);
+
+        }
+        return false;
+    }
+
+    private boolean guessLetterInTitle(String letter) {
+
+
+
+        boolean letterGuessed = false;
+        StringBuilder tempTitle = new StringBuilder(hiddenTittle);
+        for (int i = 0; i < movieTitleToGuess.length() ; i++) {
+            if (movieTitleToGuess.charAt(i) == letter.charAt(0)) {
+                tempTitle.setCharAt(i, letter.charAt(0));
+                letterGuessed = true;
+            }
+        }
+
+        hiddenTittle = tempTitle.toString();
+
+        if (letterGuessed) {
+            return true;
+        } else {
+            points--;
+            if (alreadyTypedWrongLetters.isEmpty()) {
+            alreadyTypedWrongLetters += letter;
+            } else {
+                alreadyTypedWrongLetters += ", " + letter;
+            }
+            return false;
+        }
+
+    }
+
+    private boolean isTitleGuessed() {
+        return movieTitleToGuess.equals(hiddenTittle);
+    }
+
+    private boolean wasAlreadyTyped(String letter) {
+        if (alreadyTypedLetters.isEmpty()) {
+            alreadyTypedLetters += letter;
+            return false;
+        }
+
+        for (int i = 0; i < alreadyTypedLetters.length() ; i++) {
+            if (alreadyTypedLetters.charAt(i) == letter.charAt(0)) {
+                return true;
+            }
+        }
+        alreadyTypedLetters += letter;
+        return false;
+    }
+
+    public String getMovieTitleToGuess() {
+        return movieTitleToGuess;
+    }
+
+    public void setMovieTitleToGuess(String movieTitleToGuess) {
+        this.movieTitleToGuess = movieTitleToGuess;
+    }
+
+    public String getHiddenTittle() {
+        return hiddenTittle;
+    }
+
+    public void setHiddenTittle(String hiddenTittle) {
+        this.hiddenTittle = hiddenTittle;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    private String hideTittleUderscore (String title) {
+        String uderscoreTitle = "";
+
+        for (int i = 0; i < title.length() ; i++) {
+            if (title.charAt(i) == ' ') {
+                uderscoreTitle += " ";
+            } else {
+                uderscoreTitle += "_";
+            }
+        }
+
+        return uderscoreTitle;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,52 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+//declare file
+        File file = new File("movies.txt");
+        Scanner scanner = new Scanner(file);
+
+//check number of titles in file
+        int noOfRow = 0;
+        while (scanner.hasNextLine()) {
+           noOfRow ++;
+           scanner.nextLine();
+        }
+        scanner.close();
+
+//declare array with all file titles
+        String [] allMovies = new String[noOfRow];
+
+        Scanner scanner2 = new Scanner(file);
+        for (int i = 0; i < 25 ; i++) {
+            allMovies[i] = scanner2.nextLine();
+        }
+
+//pickRandomMovie
+        String randomlyPickedMovie  = allMovies[(int) (Math.random() * 24)];
+//        System.out.printf(randomlyPickedMovie);
+
+//      create object game and pass title that has been drawn
+            Game game = new Game(randomlyPickedMovie);
+
+            // just to test
+//        System.out.println(game.getMovieTitleToGuess());
+//        System.out.println(game.getHiddenTittle());
+//        System.out.println(game.getPoints());
+
+//        Run the game
+        if (game.startGame()) {
+            System.out.println("YOU WIN!");
+        } else {
+            System.out.println("YOU LOOSE... Try again later.");
+        }
+        System.out.println("Guessed tittle was: " + game.getMovieTitleToGuess());
+
+
+        }
     }
-}
